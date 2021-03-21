7b48fc554bc9218d14256e8d9a9b96c272cf63fe
killerbomb991 <81043205+killerbomb991@users.noreply.github.com>
2021-03-21 18:09:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ function compute()
 	years = document.getElementById("years").value;
 	span = document.getElementById("result").innerHTML;
 	result;
+	if (p>0)
+	{
 	year = new Date();
 	yea = Number(year.getFullYear()) + Number(years)-1;
 	result = ((p*rate)/100)*years;
 		
-	span = 'If you deposit ' + p + ', <br/>at an interest rate of ' +rate+ '%. <br/>You will recieve an amount of '+ result + ', <br/>in the year '+ yea; 
+	span = 'If you deposit <mark>' + p + '</mark>, <br/>at an interest rate of <mark>' +rate+ '%.</mark> <br/>You will recieve an amount of <mark>'+ result + '</mark>, <br/>in the year <mark>'+ yea +'</mark>'; 
 	document.getElementById("result").innerHTML=span;
+	}
+	else
+	{
+		alert("the amount must be greater than zero");
+		document.getElementById("principal").focus();
+	}
 }
    
 function updateTextInput(val) 
